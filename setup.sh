551729cb4f5c110d350bd3743cd502d2f05fabd2
dr-macro <109559581+dr-macro@users.noreply.github.com>
2023-03-25 21:05:32 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+\n\
+[theme]\n\
+primaryColor=\"#002171\"\n\
+backgroundColor=\"#f3f4f6\"\n\
+secondaryBackgroundColor=\"#FFFFFF\"\n\
+textColor=\"#262730\"\n\
+font=\"sans serif\"\n\
+\n\
+" > ~/.streamlit/config.toml
